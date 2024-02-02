adabfee4d9d160b07ae3608c0d05cdfdf57dabc3
HannaYermolenko <158002543+HannaYermolenko@users.noreply.github.com>
2024-02-02 23:26:05 +0100

    add a new file

---
+++ HomeWork02.java
@@ -0,0 +1,20 @@
+package homework02;
+
+public class HomeWork02 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Helo Cohort 41");
+        System.out.println("Gute Nacht!");
+        String name = "Hanna";
+
+        int age = 35;
+
+        double milk = 1.9;
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("1 liter of milk: " + milk);
+    }
+}
+
+// # < > + ( ) + ! * { } : "" | , . \/ - _ ? ;
